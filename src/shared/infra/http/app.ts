393026c4e045729ca12466c1472dbae8fa33c8c9
Matheus Sartori <ms.sartori@outlook.com.br>
2022-06-29 16:04:22 -0300

    feat: add docs router (swagger ui)

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
+import 'express-async-errors'
 
 import cors from 'cors'
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
-import 'express-async-errors'
+import swaggerFile from '@shared/infra/http/docs/swagger/swagger.json'
 
 import { routes } from './routes'
 
@@ -12,5 +14,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(routes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 export { app }
