90764b14af16c80a3ec6a8bda60fb408520b0aff
Matheus Sartori <ms.sartori@outlook.com.br>
2022-06-29 15:59:06 -0300

    feat: cors on app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config'
 
+import cors from 'cors'
 import express from 'express'
 
 import 'express-async-errors'
+
 import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 export { app }
