26e02256c37c54d9cd3f4d4b6bba0d4e8cb538f9
Matheus Sartori <ms.sartori@outlook.com.br>
2022-06-29 15:57:55 -0300

    feat: main router on app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
 
 import express from 'express'
+
 import 'express-async-errors'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(routes)
 
 export { app }
